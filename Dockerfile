0d8f79b5a29445aa1acd83ded1f06f754986aac2
davwav <davidkatheking@gmail.com>
2024-04-14 20:35:53 +0400

    init commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# Use an official Python runtime as a parent image
+FROM python:3.8-slim AS builder
+
+# Set the working directory in the container
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y gcc python3-dev
+
+# Copy the requirements file
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code
+COPY . .
+
+# Runtime image
+FROM python:3.8-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy only the built dependencies from the previous stage
+COPY --from=builder /usr/local/lib/python3.8/site-packages/ /usr/local/lib/python3.8/site-packages/
+
+# Copy the application code
+COPY . .
+
+# Command to run the application
+CMD ["python", "main.py", "/output", "output"]
---
+++ main.py
@@ -0,0 +1,43 @@
+import psutil
+import json
+import os
+import sys
+
+def get_processes_info():
+    processes_info = []
+    for process in psutil.process_iter(['pid', 'name']):
+        prime_state = None
+        for i in range(2, (int(process.info['pid']/2))):
+            if ( process.info['pid'] % i == 0 ):
+                prime_state = False
+                break
+            else:
+                prime_state = True
+        if process.info['pid'] == 1:
+                prime_state = True
+        processes_info.append({
+            'Process Name': process.info['name'],
+            'Process ID': process.info['pid'],
+            'Is Prime': f'{prime_state}'
+        })
+    return processes_info
+
+def generate_json(output_dir, json_data, json_name):
+
+    data = json_data
+
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_file = os.path.join(output_dir, f"{json_name}.json")
+    with open(output_file, "w") as f:
+        json.dump(data, f, indent=4)
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python main.py OUTPUT_DIR OUTPUT_NAME ")
+        sys.exit(1)
+
+    name_of_json = sys.argv[2]
+    processes_info = get_processes_info()
+    output_dir = sys.argv[1]
+    generate_json(output_dir, processes_info, name_of_json)
